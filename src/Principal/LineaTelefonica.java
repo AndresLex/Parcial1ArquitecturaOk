88d53a9bf9b0cdb39d9e1d9f9aa36b4f6729802b
AndresLex <andrescamargocisf@gmail.com>
2023-03-18 20:09:29 -0500

    Segundo Commit

--- src/Principal/LineaTelefonica.java
+++ src/Principal/LineaTelefonica.java
@@ -1,2 +1,52 @@
-package Principal;public class LineaTelefonica {
+package Principal;
+
+import java.util.ArrayList;
+
+public class LineaTelefonica {
+    private ArrayList<Integer> duracionLlamada;
+    private double costoTotalLlamadas;
+
+    public LineaTelefonica() {
+        this.duracionLlamada = new ArrayList<>();
+        this.costoTotalLlamadas = 0;
+    }
+
+    public void  registrarLlamada(int duracion, int precio) {
+        this.duracionLlamada.add(duracion);
+        this.costoTotalLlamadas += (duracion * precio);
+    }
+
+    public int calcularDuracionTotal() {
+        int duracionTotal = 0;
+        for (Integer duracion: duracionLlamada) {
+            duracionTotal += duracion;
+        }
+        return  duracionTotal;
+    }
+    public String mostrarInformacion() {
+        return "\tNúmero de llamadas realizadas: "+this.duracionLlamada.size()+"\n" +
+               "\tDuración total de llamadas: "+this.calcularDuracionTotal()+" minutos\n" +
+               "\tCosto total de las "+this.duracionLlamada.size()+" llamadas: "+Math.round(this.costoTotalLlamadas*100.0)/100.0+" pesos";
+    }
+
+    public static String mostrarConsolidado(LineaTelefonica local, LineaTelefonica distancia, LineaTelefonica celular) {
+        double costoTotal = local.costoTotalLlamadas+distancia.costoTotalLlamadas+celular.costoTotalLlamadas;
+        int llamadasRalizadas = local.duracionLlamada.size()+distancia.duracionLlamada.size()+celular.duracionLlamada.size();
+        int duracionTotal = local.calcularDuracionTotal()+distancia.calcularDuracionTotal()+celular.calcularDuracionTotal();
+        double costoPromedio = costoTotal/duracionTotal;
+
+        return "\tCosto total de las tres líneas: "+costoTotal+" pesos\n"+
+               "\tNúmero total de llamadas realizadas: "+llamadasRalizadas+"\n"+
+               "\tDuración total de llamadas: "+duracionTotal+" minutos\n"+
+               "\tCosto promedio por minuto: "+Math.round(costoPromedio*100.0)/100.0+" pesos\n"+
+               "\tTotal de minutos: \n\t\tLocal: "+local.duracionLlamada.toString()+
+               "\n\t\tLarga Distancia: "+distancia.duracionLlamada.toString()+
+               "\n\t\tCelular: "+celular.duracionLlamada.toString();
+    }
+
+    public void reiniciar() {
+        this.costoTotalLlamadas = 0;
+        this.duracionLlamada.clear();
+    }
+
 }
--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -1,5 +1,102 @@
+package Principal;
+
+import java.util.Scanner;
+//Andres Leonardo Camargo Cortes
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*PARCIAL ARQUITECTURA DE DATOS
+        Se quiere crear una aplicación para controlar los gastos telefónicos de una empresa.
+        La empresa cuenta con tres líneas telefónicas a través de las cuales se pueden realizar llamadas:
+        locales
+        largaDistancia
+        celulares
+
+                La empresa cuenta con planes telefónicos que establece las siguientes tarifas:
+
+        Minuto Llamada Local: $60 pesos
+        Minuto Llamada Larga Distancia: $1200 pesos
+        Minuto Llamada Celular: $850 pesos
+
+        La aplicación debe permitir:
+        1. REGISTRAR una llamada en alguna de las líneas,
+
+        2. MOSTRAR la información detallada de cada línea (
+        número de llamadas realizadas
+        duración total de las llamadas en minutos
+        y el costo total de las llamadas en pesos)
+
+        3. MOSTRAR un consolidado total de la información de todas las líneas (
+                costo total en pesos de las tres líneas,
+                número total de llamadas realizadas,
+                duración total de llamadas en minutos y
+                el cálculo del costo promedio: por minuto según el costo total y
+                el total de minutos).
+
+                Adicionalmente, la aplicación debe permitir
+
+        4. REINICIAR el uso las líneas telefónicas, dejando todos sus valores en cero.*/
+
+        Scanner teclado = new Scanner(System.in);
+        LineaTelefonica local = new LineaTelefonica();
+        LineaTelefonica distancia = new LineaTelefonica();
+        LineaTelefonica celular = new LineaTelefonica();
+
+        boolean menu = true;
+        do {
+            System.out.println("Menu Lineas Telefonicas");
+            System.out.println("1.Llamada Local");
+            System.out.println("2.Llamada Larga distancia");
+            System.out.println("3.Llamada Celular");
+            System.out.println("4.Informacion Linea Local");
+            System.out.println("5.Informacion Linea Larga distancia");
+            System.out.println("6.Informacion Linea Celular");
+            System.out.println("7.Ver Consolidado de Informacion");
+            System.out.println("8.Reiniciar Lineas");
+            System.out.println("9.SALIR");
+            int opc = teclado.nextInt();
+            int minutos;
+            switch (opc){
+                case 1:
+                    System.out.println("\tRealizando Llamada Local -> Valor Minuto: $60 pesos\nRegistre cantidad de minutos: ");
+                    minutos = teclado.nextInt();
+                    local.registrarLlamada(minutos, 120);
+                    break;
+                case 2:
+                    System.out.println("\tRealizando Llamada Larga distancia -> Valor Minuto $1200 pesos\nRegistre cantidad de minutos: ");
+                    minutos = teclado.nextInt();
+                    distancia.registrarLlamada(minutos, 1200);
+                    break;
+                case 3:
+                    System.out.println("\tRealizando Llamada Larga distancia -> Valor Minuto $850 pesos\nRegistre cantidad de minutos: ");
+                    minutos = teclado.nextInt();
+                    celular.registrarLlamada(minutos, 850);
+                    break;
+                case 4:
+                    System.out.println("\tLinea Telefonica: Local");
+                    System.out.println(local.mostrarInformacion());
+                    break;
+                case 5:
+                    System.out.println("\tLinea Telefonica: Larga Distancia");
+                    System.out.println(distancia.mostrarInformacion());
+                    break;
+                case 6:
+                    System.out.println("\tLinea Telefonica: Celular");
+                    System.out.println(celular.mostrarInformacion());
+                    break;
+                case 7:
+                    System.out.println("\tConsolidado de informacion");
+                    System.out.println(LineaTelefonica.mostrarConsolidado(local, distancia, celular));
+                    break;
+                case 8:
+                    local.reiniciar();
+                    distancia.reiniciar();
+                    celular.reiniciar();
+                    System.out.println("\tLineas Reiniciadas");
+                    break;
+                case 9:
+                    System.out.println("\tGracias por usar nuestro servicio");
+                    menu = false;
+            }
+        } while (menu == true);
     }
 }
